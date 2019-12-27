404661157b6f25f42961773822c1b75aa5811139
Akash yadav <58543543+akash-yadav12@users.noreply.github.com>
2019-12-27 20:51:56 +0530

    small bug fixed

--- search.php
+++ search.php
@@ -1,7 +1,7 @@
 <html>
 	<head>
 		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-		<title>Webslesson Demo - Ajax Live Data Search using Jquery PHP MySql</title>
+		<title>COMPONENT SEARCH/LIST</title>
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
 		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
 		<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet" />
