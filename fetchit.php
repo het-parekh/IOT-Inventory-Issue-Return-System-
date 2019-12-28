be35e3763f435197210896ebf81a50636f39feb4
DAKSHIT KARSAN VAVIYA <54734141+dakshit050@users.noreply.github.com>
2019-12-28 17:08:55 +0530

    issue Display page successful

---
+++ fetchit.php
@@ -0,0 +1,45 @@
+<?php
+// $connect = mysqli_connect("localhost", "root", "", "iot_inventory");
+$con= mysqli_connect("localhost","root","pwd","iot_inventory");
+$output = '';
+if(isset($_POST["query"]))
+{
+	$search = mysqli_real_escape_string( $con,$_POST["query"]);
+	$query = "
+	SELECT * FROM issue
+    WHERE C_ID LIKE '%".$search."%'
+    AND Dept_name LIKE '%".'IT'."%'";
+}
+else
+{
+	$query = "SELECT * FROM issue WHERE Dept_name='IT'";
+}
+$result = mysqli_query($con, $query);
+if(mysqli_num_rows(mysqli_query($con,$query)) > 0)
+{
+	$output .= '<div class="table-responsive">
+					<table class="table table bordered">
+						<tr>
+                        <th>RollNo</th>
+                        <th>GroupId</th>
+                        <th>Component_ID</th>
+                        <th>Issue_Date</th>
+						</tr>';
+	while($row = mysqli_fetch_array($result))
+	{
+		$output .= '
+			<tr>
+				<td>'.$row["Rollno"].'</td>
+				<td>'.$row["Group_id"].'</td>
+				<td>'.$row["c_ID"].'</td>
+				<td>'.$row["issue_date"].'</td>
+			</tr>
+		';
+	}
+	echo $output;
+}
+else
+{
+	echo 'Component not found';
+}
+?>
