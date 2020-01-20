f910b350f9546f9d3cee9b745c22db2836888a21
DAKSHIT KARSAN VAVIYA <54734141+dakshit050@users.noreply.github.com>
2020-01-20 20:56:30 +0530

    cookies added

--- profile.php
+++ profile.php
@@ -1,24 +1,10 @@
 <?php
-session_start();
 if(isset($_COOKIE['username'])){
     $name=$_COOKIE['username'];
-    echo"Welcome Mr.$name";
-}else{
-    echo "<script>alert('sorry')</script>";
-    echo "<script>location.href='logout.php'</script>";
+    include_once("dashbord-new.php");
 }
-
+ 
+else{
+    echo "<script>location.href='loginpro7.php'</script>";
+}
 ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-    <a href="logout.php">logout</a>
-    
-</body>
-</html>
